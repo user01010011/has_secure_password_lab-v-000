44be7f966dab96200f4eca3d9978bfafcffa304d
Shasha Yan <shashayan@gmail.com>
2021-06-15 21:15:29 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:new, :create]
+  root "welcome#home"
+  get "/login" => "sessions#new"
+  post "/login" => "sessions#create" 
+  post "/logout" => "sessions#destroy"
+  
+
 end
